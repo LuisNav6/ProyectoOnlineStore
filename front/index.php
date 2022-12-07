47a46f6d0bfda67afbc321e3501e140e05a54bbd
Luis123 <luishnb4@gmail.com>
2022-12-06 22:50:28 -0600

    se Agregaron productos y primera parte carrito

--- front/index.php
+++ front/index.php
@@ -1,3 +1,16 @@
+<?php
+
+$servidor='localhost'; //todos estos datos se cambiarán acorde a la bd que se vaya a usar
+$cuenta='id19790264_root';
+$password='Productosdatabase1.';
+$bd='id19790264_dbproductos';
+
+$conexion = new mysqli($servidor,$cuenta,$password,$bd);
+
+$sql = 'select * from productos';
+$resultado = $conexion -> query($sql);
+
+?>
 <!DOCTYPE html>
 <html lang="es">
 
@@ -11,10 +24,10 @@
     <link rel="icon" type="image/png" href="/favicon.png" />
     <link rel="icon" type="" href="" />
 
-
-
     <!-- Box Icons -->
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
     <!-- Link To CSS -->
     <link rel="stylesheet" href="styles.css">
 
@@ -33,6 +46,7 @@
             <li><a href="#home">Home</a></li>
             <li><a href="#about">Acerca de nosotros</a></li>
             <li><a href="#menu">Productos</a></li>
+            <li><a href="#menu">Tienda</a></li>
             <li><a href="#services">Servicio</a></li>
             <li><a href="#contact">Contacto</a></li>
             <li><a href="#" class="login">Iniciar sesión</a></li>
@@ -74,6 +88,7 @@
                 modal.classList.remove('modal--show');
             });
         </script>
+
     </header>
 
 
@@ -110,7 +125,7 @@
     <section class="menu" id="menu">
         <div class="heading">
             <span>Productos</span>
-            <h2>Lista de productos</h2>
+            <h2>¡Lo más nuevo!</h2>
         </div>
         <div class="menu-container">
             <!-- Box 1 -->
@@ -126,35 +141,124 @@
             <!-- Box 2 -->
             <div class="box">
                 <div class="box-img">
-                    <img src="Img/c++.jpg" alt="">
+                    <img src="Img/pantsHombre.jpeg" alt="">
                 </div>
-                <h2></h2>
+                <h2>Pants Adidas Hombre</h2>
                 <h3></h3>
-                <span>$</span>
+                <span>$1499.00</span>
                 <i class='bx bx-cart-alt'></i>
             </div>
             <!-- Box 3 -->
             <div class="box">
                 <div class="box-img">
-                    <img src="Img/php.jpg" alt="">
+                    <img src="Img/bandasDama.jpeg" alt="">
                 </div>
-                <h2></h2>
+                <h2>Bandas Multicolores para Dama</h2>
                 <h3></h3>
-                <span></span>
+                <span>$679.00</span>
                 <i class='bx bx-cart-alt'></i>
             </div>
             <!-- Box 4 -->
             <div class="box">
                 <div class="box-img">
-                    <img src="Img/javascript.jpg" alt="">
+                    <img src="Img/playeraNiño.jpeg" alt="">
                 </div>
-                <h2></h2>
+                <h2>Playera Niño</h2>
                 <h3></h3>
-                <span></span>
+                <span>$650.00</span>
                 <i class='bx bx-cart-alt'></i>
             </div>
         </div>
     </section>
+    <!-- tienda-->
+    <section class="menu" id="menu">
+        <div class="heading">
+            <span>Tienda</span>
+            <h2>Encuentra lo mejor para ti.</h2>
+        </div>
+        <div class="menu-container">
+        <?php
+          $numPro = 0;
+        ?>
+<script> var array=[];var arr=[]; var ar=[];</script>
+ 
+<?php
+       srand(time());
+       $x = rand(0,1);//el limite maximo es el numero total de articulos
+    while( $fila = $resultado ->  fetch_assoc()){
+        $imagen = $fila['imagen'];
+        $producto = $fila['nombre'];
+        $precio = $fila['precio'];
+        $descripcion = $fila['descripcion']; 
+        $categoria = $fila['categoria'];
+?>
+    <script>
+    array.push("<?php echo $producto ?>");
+    ar.push("<?php echo $categoria ?>");
+    </script>
+
+<?php 
+     if($numPro == $x){
+         $newprecio = $precio*0.9;
+?>
+<script>
+     arr.push("<?php echo $newprecio ?>");
+</script>
+            <div class="box">
+                <div class="box-img">
+                    <img src="Img/<?php echo $imagen ?>">
+                </div>
+                <h2><?php echo $producto ?></h2>
+                <h3></h3>
+                <span style="color: gray;"><del>$<?php echo $precio ?></del></span>
+                <span style="color: green;"><?php echo $newprecio?></span>
+                <button id="<?php echo $numPro ?>" onclick="agregar(this.id)">
+                <i class='bx bx-cart-alt'></i>
+                 </button>
+            </div>
+        </form>
+<?php
+     }else{
+?>
+<script>
+     arr.push("<?php echo $precio ?>");
+</script>
+            <div class="box">
+                <div class="box-img">
+                    <img src="Img/<?php echo $imagen ?>">
+                </div>
+                <h2><?php echo $producto ?></h2>
+                <h3></h3>
+                <span>$<?php echo $precio ?></span>
+                <button id="<?php echo $numPro ?>" onclick="agregar(this.id)">
+                <i class='bx bx-cart-alt'></i>
+                 </button>
+            </div>
+            </form>
+        </div>
+    </section>
+
+<?php } ?>
+<?php
+    $numPro = $numPro+1;
+    }//fin while
+?>
+
+<script>
+    console.log(array); 
+    console.log(arr);
+    console.log(ar);
+    let carrito = [];
+    let carritochido = [];
+    function agregar(id){
+        var indice = parseInt(id);
+        console.log(`Elegiste ${array[indice]}`);
+       carrito.push(`${array[indice]}`,`${arr[indice]}`,`${ar[indice]}`);
+    }
+    carritochido.push(carrito);
+    console.log(carritochido);
+    console.log(carrito);
+</script>
 
     <!-- Service -->
     <section class="services" id="services">
@@ -168,8 +272,8 @@
             <div class="s-box">
                 <img src="Img/seguridadpago.jpg" alt="">
                 <h3>Pagos</h3>
-                <p>Contamos con una de las pasarelas mas seguras en linea, que garantiza a nuestros clientes que su
-                    informacion personal y sus datos bancarios están 100% protegidos.</p>
+                <p>Contamos con una de las pasarelas más seguras en línea, que garantiza a nuestros clientes que su
+                    información personal y sus datos bancarios estén 100% protegidos.</p>
             </div>
             <!-- Box 2 -->
             <div class="s-box">
@@ -240,14 +344,11 @@
         <p>© Derechos Reservados Por SPORTRNR.</p>
     </div>
 
-
-
+    <!-- boxicons js-->
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
     <!-- Scroll Reveal -->
     <script src="https://unpkg.com/scrollreveal"></script>
     <!-- Link To JavaScript -->
     <script src="script.js"></script>
 </body>
-
-
-
 </html>
